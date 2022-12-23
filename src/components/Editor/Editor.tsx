94cf72176d020d3d63401fd463ad1e0209920039
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-23 08:05:56 +0300

    Added zoom functionality to the editor

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './Editor.scss';
 import { EditorState } from '@codemirror/state';
 import { EditorView, keymap } from '@codemirror/view';
@@ -6,15 +6,22 @@ import { indentWithTab } from '@codemirror/commands';
 import { basicSetup } from 'codemirror';
 import { oneDark } from '@codemirror/theme-one-dark';
 import { javascript } from '@codemirror/lang-javascript';
+import { ZoomInOutlined, ZoomOutOutlined } from '@mui/icons-material';
 
 export const Editor = () => {
+	const [code, setCode] = useState('');
+	const [fontSize, setFontSize] = useState(12);
 	const editorRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
 		const FontSizeTheme = EditorView.theme({
 			'&': {
-				fontSize: '12pt',
+				fontSize: `${fontSize}pt`,
 			},
+		});
+
+		const onUpdate = EditorView.updateListener.of((value) => {
+			setCode(value.state.doc.toString());
 		});
 
 		const editorStartState = EditorState.create({
@@ -23,9 +30,10 @@ export const Editor = () => {
 				basicSetup,
 				keymap.of([indentWithTab]),
 				oneDark,
-				javascript(),
 				[FontSizeTheme],
-			], // keymaps are all the editor keymaps like Ctrl+V
+				javascript({ jsx: true, typescript: true }),
+				onUpdate,
+			],
 		});
 
 		const editorView = new EditorView({
@@ -36,7 +44,28 @@ export const Editor = () => {
 		return () => {
 			editorView.destroy();
 		};
-	}, []);
+	}, [fontSize]);
 
-	return <div ref={editorRef} className='w-full min-h-screen'></div>;
+	return (
+		<div ref={editorRef} className='w-full min-h-screen relative'>
+			<div className='absolute top-1 right-1 z-10 w-auto'>
+				<button
+					onClick={() => setFontSize((prev) => prev + 1)}
+					className='px-2 py-0.5 mr-0.5 rounded-l-lg bg-teal-300 text-center opacity-30 hover:bg-teal-500 hover:opacity-100'
+				>
+					<span className='text-white text-md font-semibold'>
+						<ZoomInOutlined />
+					</span>
+				</button>
+				<button
+					onClick={() => setFontSize((prev) => prev - 1)}
+					className='px-2 py-0.5 rounded-r-lg bg-teal-300 text-center opacity-30 hover:bg-teal-500 hover:opacity-100'
+				>
+					<span className='text-white text-md font-semibold'>
+						<ZoomOutOutlined />
+					</span>
+				</button>
+			</div>
+		</div>
+	);
 };
