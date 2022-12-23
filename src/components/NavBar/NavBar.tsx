cc6b0dd94a002ce9d3126d3c17234b68fabe6167
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-23 15:18:11 +0300

    Created a custom language selector

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,13 +2,26 @@ import { PlayArrowOutlined } from '@mui/icons-material';
 import './NavBar.sass';
 import { Selector } from '../Selector';
 
+const imagesFolder = `${process.env.PUBLIC_URL}/assets/images`;
+
+const languages = [
+	{ name: 'C', value: 'c', icon: `${imagesFolder}/c.svg` },
+	{ name: 'C++', value: 'cpp', icon: `${imagesFolder}/cpp.svg` },
+	{ name: 'C#', value: 'cs', icon: `${imagesFolder}/cs.svg` },
+	{ name: 'Java', value: 'java', icon: `${imagesFolder}/java.svg` },
+	{ name: 'JavaScript', value: 'js', icon: `${imagesFolder}/js.svg` },
+	{ name: 'Python', value: 'py', icon: `${imagesFolder}/py.svg` },
+	{ name: 'TypeScript', value: 'ts', icon: `${imagesFolder}/ts.svg` },
+	{ name: 'Ruby', value: 'rb', icon: `${imagesFolder}/rb.svg` },
+];
+
 export const NavBar = () => {
 	return (
-		<div className='flex justify-between align-middle items-center w-full bg-teal-400 text-white px-4 py-2'>
+		<div className='flex justify-between align-middle items-center w-full bg-teal-600 text-white px-4 py-2'>
 			<h5 className='font-bold text-2xl'>
 				<span className='mr-1'>{'</>'}</span>CodeYetu
 			</h5>
-			<Selector />
+			<Selector options={languages} />
 			<button className='px-2 py-1 h-10 rounded-lg bg-red-400 text-white'>
 				Run <PlayArrowOutlined />
 			</button>
--- src/components/Selector/Selector.tsx
+++ src/components/Selector/Selector.tsx
@@ -1,31 +1,102 @@
+import { useEffect, useState } from 'react';
 import './Selector.sass';
+import {
+	ExpandLessOutlined,
+	ExpandMoreOutlined,
+	SearchOutlined,
+} from '@mui/icons-material';
 
-export const Selector = () => {
-	const languages = [
-		{ name: 'C', code: 'c' },
-		{ name: 'C++', code: 'cpp' },
-		{ name: 'C#', code: 'cs' },
-		{ name: 'Java', code: 'java' },
-		{ name: 'JavaScript', code: 'js' },
-		{ name: 'Python', code: 'py' },
-		{ name: 'TypeScript', code: 'ts' },
-		{ name: 'Ruby', code: 'rb' },
-	];
+interface SelectorOption {
+	name: string;
+	value: string;
+	icon?: string;
+}
+
+interface SelectorProps {
+	options?: Array<SelectorOption>;
+	selected?: string;
+}
+
+export const Selector: React.FC<SelectorProps> = ({ options }) => {
+	const [open, setOpen] = useState(false);
+	const [search, setSearch] = useState('');
+	const [selected, setSelected] = useState<SelectorOption>();
+
+	useEffect(() => {
+		const index = Math.floor(Math.random() * (options?.length || 0));
+		setSelected(options && options[index]);
+	}, []);
 
 	return (
-		<select
-			name='language'
-			id='langselect'
-			className='appearance-none m-0 px-4 py-0 text-lg font-normal text-white bg-inherit bg-clip-padding bg-no-repeat border-b-2 border-solid border-white focus:outline-none'
-		>
-			<option value='' selected>
-				Select Language
-			</option>
-			{languages.map((lang, idx) => (
-				<option key={idx} value={lang.code}>
-					{lang.name}
-				</option>
-			))}
-		</select>
+		<div className='w-52 font-medium cursor-pointer'>
+			<div
+				onClick={() => setOpen(!open)}
+				className={`bg-inherit w-full flex justify-around items-center border-b-2 border-solid border-b-white relative ${
+					!selected && 'text-white'
+				}`}
+			>
+				<p className='py-2'>
+					{selected?.name ? (
+						selected.name.length > 25 ? (
+							`${selected.name.substring(0, 15)}...`
+						) : (
+							<div className='flex items-center justify-between'>
+								{selected.icon && (
+									<img src={selected.icon} alt={`${selected.name} icon`} />
+								)}
+								<p className='text-lg font-semibold'>{selected.name}</p>
+							</div>
+						)
+					) : (
+						'Select Language'
+					)}
+				</p>
+				{!open ? <ExpandMoreOutlined /> : <ExpandLessOutlined />}
+			</div>
+			<ul
+				className={`bg-white mt-2 overflow-y-auto rounded absolute top-12 z-10 w-52 text-gray-800  ${
+					open ? 'max-h-60' : 'max-h-0'
+				}`}
+			>
+				<>
+					<div className='flex items-center px-2 sticky top-0 bg-white text-gray-700'>
+						<SearchOutlined />
+						<input
+							type='text'
+							value={search}
+							onChange={(e) => setSearch(e.target.value.toLowerCase())}
+							placeholder='Enter language'
+							className='placeholder:text-gray-500 p-2 outline-none w-full'
+						/>
+					</div>
+					{options?.map((option, idx) => (
+						<li
+							key={idx}
+							className={`p-2 text-sm hover:bg-teal-600 hover:text-white ${
+								option.value === selected?.value && 'bg-teal-600 text-white'
+							} ${
+								option.name.toLowerCase().startsWith(search.toLowerCase())
+									? 'block'
+									: 'hidden'
+							}`}
+							onClick={() => {
+								if (option.value !== selected?.value) {
+									setSelected(option);
+									setOpen(false);
+									setSearch('');
+								}
+							}}
+						>
+							<div className='flex items-center justify-between'>
+								{option.icon && (
+									<img src={option.icon} alt={`${option.name} icon`} />
+								)}
+								<p className='text-lg font-semibold'>{option.name}</p>
+							</div>
+						</li>
+					))}
+				</>
+			</ul>
+		</div>
 	);
 };
