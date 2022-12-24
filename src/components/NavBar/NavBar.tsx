7faf6fa7ac449880768800f946ccb77a18307027
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-24 08:12:05 +0300

    Made some aesthetic improvements

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -18,11 +18,9 @@ const languages = [
 export const NavBar = () => {
 	return (
 		<div className='flex justify-between align-middle items-center w-full bg-teal-600 text-white px-4 py-2'>
-			<h5 className='font-bold text-2xl'>
-				<span className='mr-1'>{'</>'}</span>CodeYetu
-			</h5>
+			<h5 className='font-bold text-2xl'>CodeYetu</h5>
 			<Selector options={languages} />
-			<button className='px-2 py-1 h-10 rounded-lg bg-red-400 text-white'>
+			<button className='px-2 py-1 h-10 rounded-lg bg-red-400 text-white hover:bg-red-500'>
 				Run <PlayArrowOutlined />
 			</button>
 		</div>
